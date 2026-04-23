275848c148969038b3c715183294bbdc20876c2e
Maxim-Coder-arch <maximnovikov2206@gmail.com>
2026-04-23 13:55:10 +0300

    fix styles

--- app/components/mini-catalog/miniCatalog.tsx
+++ app/components/mini-catalog/miniCatalog.tsx
@@ -7,7 +7,10 @@ import "./index.scss";
 import SeriesCard from "@/app/share/series-card/seriesCard";
 import TemplateCards from "@/app/share/templateCards/templateCard";
 
-
+const decorData = [
+  { _id: "decor1", image: "https://sun9-21.userapi.com/s/v1/ig2/GdbJCGArz3DoDBqi9wZ6kWcmZLg-3ZSIaSgFKw_jMtmhoSCdJzoz7lhOTe7JxqRCjH5CEe2zUD2FfDjoiIZR84uV.jpg?quality=95&crop=0,17,941,1411&as=32x48,48x72,72x108,108x162,160x240,240x360,360x540,480x720,540x810,640x960,720x1080,941x1411&from=bu&u=UsUDD9GmXTGY0ccPKlBbxos0fPH_MvgH-NiFJl3ncS8&cs=941x0", title: "Розочки", link: "/decor/1" },
+  { _id: "decor2", image: "https://sun9-21.userapi.com/s/v1/ig2/GdbJCGArz3DoDBqi9wZ6kWcmZLg-3ZSIaSgFKw_jMtmhoSCdJzoz7lhOTe7JxqRCjH5CEe2zUD2FfDjoiIZR84uV.jpg?quality=95&crop=0,17,941,1411&as=32x48,48x72,72x108,108x162,160x240,240x360,360x540,480x720,540x810,640x960,720x1080,941x1411&from=bu&u=UsUDD9GmXTGY0ccPKlBbxos0fPH_MvgH-NiFJl3ncS8&cs=941x0", title: "Стразы", link: "/decor/2" },
+];
 
 const MiniCatalog = () => {
   const [activeCategory, setActiveCategory] = useState<"ready" | "decor">("ready");
@@ -36,9 +39,13 @@ const MiniCatalog = () => {
             Декоры
           </button>
         </div>
+        
         <TemplateCards>
           {activeCategory === "ready" && data.map((product) => (
             <SeriesCard product={product} key={product._id} />
+          ))}
+          {activeCategory === "decor" && decorData.map((item) => (
+            <SeriesCard product={item} key={item._id} /> // нужно будет сделать DecorCard
           ))}
         </TemplateCards>
       </div>
